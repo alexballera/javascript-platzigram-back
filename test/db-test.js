d4aa7aa061c0a058239ddd32a3bd9b5282c3b378
Alex Ballera <alexballera@gmail.com>
2017-01-08 13:40:39 -0400

    Funcionalidad para obtener todas las imagenes

--- test/db-test.js
+++ test/db-test.js
@@ -5,27 +5,29 @@ import r from 'rethinkdb'
 import Db from '../'
 import fixtures from './fixtures'
 
-const dbName = `platzigram_${uuid.v4()}`
-const db = new Db({ db: dbName })
-
 // Hooks
-test.before('setup database', async t => {
+test.beforeEach('setup database', async t => {
+  const dbName = `platzigram_${uuid.v4()}`
+  const db = new Db({ db: dbName })
   await db.connect()
+  t.context.db = db
+  t.context.dbName = dbName
   t.true(db.connected, 'shold be connected')
 })
 
-test.after('disconnect database', async t => {
+test.afterEach.always('cleanup database', async t => {
+  let db = t.context.db
+  let dbName = t.context.dbName
   await db.disconnect()
   t.false(db.connected, 'shold be disconnected')
-})
 
-test.after.always('cleanup database', async t => {
   let conn = await r.connect({})
   await r.dbDrop(dbName).run(conn)
 })
 
 // TEST
 test('save image', async t => {
+  let db = t.context.db
   t.is(typeof db.saveImage, 'function', 'saveImage is a function')
 
   let image = fixtures.getImage()
@@ -43,6 +45,7 @@ test('save image', async t => {
 })
 
 test('like image', async t => {
+  let db = t.context.db
   t.is(typeof db.likeImage, 'function', 'likeImage is a function')
 
   let image = fixtures.getImage()
@@ -54,6 +57,7 @@ test('like image', async t => {
 })
 
 test('get image', async t => {
+  let db = t.context.db
   t.is(typeof db.getImage, 'function', 'getImage is a function')
 
   let image = fixtures.getImage()
@@ -61,4 +65,14 @@ test('get image', async t => {
   let result = await db.getImage(created.public_id)
 
   t.deepEqual(created, result)
+})
+
+test('list all images', async t => {
+  let db = t.context.db
+  let images = fixtures.getImages()
+  let saveImages = images.map(img => db.saveImage(img))
+  let created = await Promise.all(saveImages)
+  let result = await db.getImages()
+
+  t.is(created.length, result.length)
 })
